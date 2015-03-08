d0f54a38bcaaff0b6ae60cf3006615f66798b153
B3nac <Rockapparatus@yahoo.com>
2015-03-07 22:18:22 -0800

    resync

--- SpaceRiot.py
+++ SpaceRiot.py
@@ -33,15 +33,16 @@ class Control(object):
         self.level = 1
         self.high_score = 0
         self.shields = 0
+        self.explosion.rect = 0
         self.explosions = []
-        self.explosion = Explosion(self.screen, Vector(200, 200), max_power=5, max_radius=35)
-        
+        self.explosion = Explosion(self.screen, Vector(0, 0), max_power=5, max_radius=35)
+
     def get_image(self, _path):
-        image = self._image_library.get(_path)
+        image = self._image_library.get(_path) 
         if image == None:
             butt_path = _path.replace('/', os.sep).replace('\\', os.sep)
             image = pg.image.load(butt_path).convert_alpha()
-            self._image_library[butt_path] = image    
+            self._image_library[butt_path] = image
         return image
 		#Not finished
 
@@ -50,12 +51,12 @@ class Control(object):
         screen = pg.display.set_mode(size)
         #Loop until the user clicks the close button.
         done=False
-        
+
         clock=pg.time.Clock()
- 
+
         display_instructions = True
         instruction_page = 1
- 
+
         # -------- Instruction Page Loop -----------
         while done==False and display_instructions:
             for event in pg.event.get(): #User did something
@@ -71,14 +72,14 @@ class Control(object):
             screen.fill(a.BLACK)
             screen.blit(self.get_image('resource/images/menubg.png'), [0,0])
             if instruction_page == 1:
-         
+
                 text = self.font.render("Click to start. WASD controls and Spacebar to shoot.", True, a.WHITE)
                 screen.blit(text, [70, 300])
-         
+
             clock.tick(20)
- 
-            pg.display.flip()       
-        
+
+            pg.display.flip()
+
     def highScore(self):
         """ Main program is here. """
         try:
@@ -103,8 +104,8 @@ class Control(object):
             #Hm, can't write it.
                print("Too bad I couldn't save it.")
         else:
-            print("Better luck next time.")   
-        
+            print("Better luck next time.")
+
     def setup_sprites(self):
         """Create all our sprite groups."""
         self.all_sprites = pg.sprite.Group()
@@ -116,8 +117,8 @@ class Control(object):
         self.make_blocks(10)
         self.make_enemies(3)
         self.make_spowerup(1)
-        
-      
+
+
     def make_blocks(self, amount):
         """Create block instances, placing them in the appropriate groups."""
         for i in range(amount):
@@ -131,16 +132,17 @@ class Control(object):
             x = random.randrange(a.SCREEN_WIDTH-self.get_image('resource/images/enemy.png').get_width())
             y = random.randrange(50)
             enemies.Enemy(self.get_image('resource/images/enemy.png'), (x,y), self.enemies, self.all_sprites)
-            
+
     def make_spowerup(self, amount):
         """Create block instances, placing them in the appropriate groups."""
         for i in range(amount):
             x = random.randrange(a.SCREEN_WIDTH-self.get_image('resource/images/s.png').get_width())
             y = random.randrange(10)
             speedpowerup.Powerup(self.get_image('resource/images/s.png'), (x,y), self.spowerup, self.all_sprites)
-                            
+
     def collision(self):
         collide = pg.sprite.spritecollideany(self.player,self.blocks)
+        
         if collide:
             EXPLO.play()
             self.score -= 10
@@ -160,13 +162,13 @@ class Control(object):
             self.score -= 40
             self.health -= 4
             self.timer = pg.time.get_ticks()
-            collide.kill()    
+            collide.kill()
         if self.health <= 0:
             self.lives -= 1
             self.health += 10
         if self.lives <= 0:
             self.highScore()
-            
+
     def collision3(self):
         collide = pg.sprite.spritecollideany(self.player, self.spowerup)
         if collide:
@@ -174,14 +176,14 @@ class Control(object):
             self.player.speed += 10
             self.timer = pg.time.get_ticks()
             collide.kill()
-            
-        
-            
+
+
+
     def event_loop(self):
         """Event loop for program; there can be only one."""
-       	      
+
         for event in pg.event.get():
-            
+
             if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
                 self.done = True
             if event.type == pg.KEYDOWN and event.key == pg.K_f:
@@ -191,15 +193,15 @@ class Control(object):
                 if self.keys[pg.K_SPACE]:
                     self.player.shoot(self.bullets, self.all_sprites)
                     LASER.play()
-            
+
             for self.explosion in self.explosions:
                 if not self.explosion.is_alive:
                     self.explosions.remove(self.explosion)
                     continue
-                                      
+
     def update(self):
         """Update all sprites and check collisions."""
-        self.all_sprites.update(self.keys, self.screen_rect) 
+        self.all_sprites.update(self.keys, self.screen_rect)
         self.collision()
         self.collision2()
         self.collision3()
@@ -208,7 +210,7 @@ class Control(object):
         hits2 = pg.sprite.groupcollide(self.bullets, self.enemies, True, True)
         for hit in hits:
             self.explosion.build(20)
-            self.explosions.append(self.explosion) 
+            self.explosions.append(self.explosion)
             EXPLO.play()
             self.score += 10
         for hit in hits2:
@@ -221,7 +223,7 @@ class Control(object):
             self.fps += 5
         if self.lives == 0:
             main()
-            
+
     def draw(self):
         """Draw all items to the screen."""
         self.screen.fill(a.WHITE)
@@ -237,16 +239,16 @@ class Control(object):
         self.screen.blit(text, (10,65))
         text = self.font.render("Highscore: {}".format(self.high_score), True, a.WHITE)
         self.screen.blit(text, (550,5))
-        
+
     def main_loop(self):
 
-        while not self.done: 
+        while not self.done:
             self.event_loop()
             self.update()
             self.draw()
             pg.display.update()
             self.clock.tick(self.fps)
-        
+
 def main():
 
     global LASER, EXPLO
